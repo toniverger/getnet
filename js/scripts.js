f838e2cccf1ff2c49b75d7f46059672740fd233e
Diego De Dieu <ddedieu@bitskingdom.com>
2021-06-21 00:09:09 -0300

    Anclas

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
+function scrollToAnchor(id){
+    var tag = $("#" + id);
+    $('html,body').animate({scrollTop: tag.offset().top - 80}, 1500);
+}
+
 $(window).on('load', function() {
 	$('.menuBtn').on('click', function(e) {
 		e.preventDefault();
 		$('body').toggleClass('openMenu');
 		$('.menuMobile').slideToggle();
-	})
+	});
+
+	$('.scrollToAnchor').on('click', function(e) {
+		e.preventDefault();
+		var id = $(this).data('anchor');
+		scrollToAnchor(id);
+	});
 })
