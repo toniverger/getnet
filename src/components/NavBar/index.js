378402f1461b0ae1194a99674800961ed4a7ba81
carolainseija <carolainsilva1@gmail.com>
2021-06-23 23:16:22 -0300

    structure basic

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
-const navBar = ()=> {
-    return(
-        <div>
-        </div>
-    )
-}
+const navBar = () => {
+  return <div></div>;
+};
 
 export default navBar;
