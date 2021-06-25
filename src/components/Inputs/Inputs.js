081e678c81a206c9337c79bd2f4995b38f4f8f5f
carolainseija <carolainsilva1@gmail.com>
2021-06-25 12:14:41 -0300

    type inputs and captcha

--- src/components/Inputs/Inputs.js
+++ src/components/Inputs/Inputs.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Inputs = (props) => {
     return(
-        <input className="inputForm" placeholder={props.placeholder} required></input>
+        <input className="inputForm" placeholder={props.placeholder} type={props.type} required></input>
     )
 }
 
