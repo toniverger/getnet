4a5913925c3cfc7a865c6560d37b5991b393bdee
carolainseija <carolainsilva1@gmail.com>
2021-06-27 21:24:58 -0300

    new changes in structure

---
+++ src/components/Texts/Text.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+const Text = ({title, subtitle}) => {
+    return(
+        <div>
+              <h1 className="title">{title}</h1>
+      <p className="subtitle">
+       {subtitle}
+      </p>
+        </div>
+    )
+}
+export default Text;
--- src/pages/Validate.js
+++ src/pages/Validate.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Pages from '../components/structure/Pages';
+
+import { Form } from '../components/Form/Form';
+import Steps from '../components/Steps/Steps';
+import empresa from "../images/icon-empresa-active.svg";
+import validar from "../images/icon-validar-active.svg";
+import confirmar from "../images/icon-confirm-disabled.svg";
+import Text from '../components/Texts/Text';
 
 export const Validate = ()=> {
     return(
         <div>
-            <h1>page validate</h1>
+            <Pages  content={
+                  <>
+                  <Steps empresa={empresa} validar={validar} confirmar={confirmar} />
+                  <Text title="Ingresa el código que te enviamos a tu celular" subtitle="Lo enviamos al 090 000 000 para confirmar que te pertenece" />
+                  </>
+            }/>
         </div>
     )
 }
