d3578a2e1446f72eb236243f7bd4a47845645f7b
carolainseija <carolainsilva1@gmail.com>
2021-06-28 09:55:48 -0300

    remove page confirm

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route,} from "react-router-dom";
 
-import Confirm from "./pages/Confirm";   
 import { Datas } from "./pages/Datas";
 import { Validate } from "./pages/Validate";
 
 function App() {
   return (
     <div>
-      <pageValidate></pageValidate>
       <Router>
         <Switch>
-          <Route path="confirm" component={Confirm} />
+         
          <Route path="/validate" component={Validate} />
           <Route path="/" component={Datas} />
         </Switch>
