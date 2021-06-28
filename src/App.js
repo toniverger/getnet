0080bdabcfa3d49020a82853227cc62ea6a9d370
carolainseija <carolainsilva1@gmail.com>
2021-06-28 12:31:32 -0300

    add images and page adress.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
+import Adress from "./pages/Adress";
 import Confirm from "./pages/Confirm";
 import { Datas } from "./pages/Datas";
 import { Validate } from "./pages/Validate";
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Router>
         <Switch>
+          <Route path="/adress" component={Adress} />
           <Route path="/validate" component={Validate} />
           <Route path="/confirm" component={Confirm} />
           <Route path="/" component={Datas} />
