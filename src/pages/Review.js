b5b83bff9751d985a329002f1aae3b6e8ddc7d45
carolainseija <carolainsilva1@gmail.com>
2021-06-28 13:01:10 -0300

    page review

---
+++ src/pages/Review.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Text from '../components/Texts/Text';
+
+const Review = () => {
+    return(
+        <div>
+            <Text title="¡Felicidades! Estamos revisando tus datos" subtitle="Esto puede demorar entre 24 y 48 horas. Cuando esté todo listo te confirmaremos por las vías de contacto solicitadas." />
+        </div>
+    )
+}
+
+export default Review;
