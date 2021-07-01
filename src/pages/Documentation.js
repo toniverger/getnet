27f01ee355adeef660140ce2e829d930c5121866
carolainseija <carolainsilva1@gmail.com>
2021-07-01 08:47:01 -0300

    structure page documentation

--- src/pages/Documentation.js
+++ src/pages/Documentation.js
@@ -7,17 +7,28 @@ import { Button } from '../components/Buttons/continue/Button';
 import infor from "../images/icon-infor.svg";
 import docs from "../images/icon-docs-active.svg";
 import revision from "../images/icon-revision.svg";
+import info from '../images/info.svg';
 
-const Documentation =  ( ) => {
-    return(
-        <Pages  content={
+const Documentation = () => {
+    return (
+        <Pages content={
             <>
-            <Steps text1="4 - Información del domicilio y operativa de venta" text2="5 - Rubro, información fiscal y documentación" text3="6- Revisión" src1={infor} src2={docs} src3={revision} />
-           <Text title="Rubro, información fiscal y documentación" />
-           <div className="container-btnContinue">
-                    <Button to="/review" text="Continue >" />
+                <Steps text1="4 - Información del domicilio y operativa de venta" text2="5 - Rubro, información fiscal y documentación" text3="6- Revisión" src1={infor} src2={docs} src3={revision} />
+                <Text title="Rubro, información fiscal y documentación" />
+                <p className="black">Rubro:</p>
+                <input className="input-select" placeholder="Seleccioná Rubro *"></input>
+                <input className="input-select" placeholder="Seleccioná Sub-rubro *"></input>
+                <input placeholder=""></input>
+                <input type="checkbox" ></input>
+                <label className="gray font18">Aplica Ley Nº 17.934 i</label>
+                <p className="black">Tipo de contribuyente:</p>
+                <img src={info} alt="icon-info" />
+                <p className="gray">Tengo declaración jurada por exoneración</p>
+                <p className="black font18">Adjuntar foto de tarjeta de RUT o Formulario DGI 6361 - 6161 - 6351: *</p>
+                <div className="container-btnContinue">
+                    <Button to="/review" text="Finalizar >" />
                 </div>
-           </>
+            </>
         } />
     )
 }
