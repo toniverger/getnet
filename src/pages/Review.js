0ce2d2b9ab62ce817aa2e5bcc0d2297dfcb816bd
carolainseija <carolainsilva1@gmail.com>
2021-07-01 09:38:16 -0300

    review page

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -6,20 +6,29 @@ import facebook from '../images/icon-fb-white.svg';
 import linkedin from '../images/icon-in-white.svg';
 
 const Review = () => {
-    return(
+    return (
         <div className="page-review">
-       <h1 className="white congrats">¡Felicidades! <br></br> Estamos revisando tus datos</h1>
-        <h2 className="white font18">Esto puede demorar entre 24 y 48 horas. Cuando esté todo listo te confirmaremos por las vías de contacto solicitadas.</h2>
-        <button className="btn-back white">Volver al inicio</button>
-        <p className="white font18">¿Tenés dudas? Contactanos al</p>
-        <img src={wpp}  alt="icon-whatsApp"/>
-        <p className="tel white font18">0000 00 00</p>
-        <img src={tel}  alt="icon-telphone"/>
-        <p className="tel white font18">0000 00 00</p>
-        <p className="white font18">Seguinos</p>
-        <img src={facebook} alt="icon-facebook" />
-        <img src={linkedin} alt="icon-linkedin" />
-        <img src={instagram} alt="icon-instagram" />
+            <div className="review">
+                <h1 className="white congrats ">¡Felicidades! <br></br> Estamos revisando tus datos</h1>
+                <p className="delay white font18">Esto puede demorar entre 24 y 48 horas.</p>
+                <p className="delay white font18">Cuando esté todo listo te confirmaremos por las vías de contacto solicitadas.</p>
+                <button className="btn-back white">Volver al inicio</button>
+            </div>
+            <div className="review-contact">
+                <div className="questions">
+                    <p className="white font18">¿Tenés dudas? Contactanos al</p>
+                    <img src={wpp} alt="icon-whatsApp" />
+                    <p className="tel white font18">0000 00 00</p>
+                    <img src={tel} alt="icon-telphone" />
+                    <p className="tel white font18">0000 00 00</p>
+                </div>
+                <div className="socials">
+                    <p className="white font18">Seguinos</p>
+                    <img className="img-socials" src={facebook} alt="icon-facebook" />
+                    <img className="img-socials" src={linkedin} alt="icon-linkedin" />
+                    <img className="img-socials" src={instagram} alt="icon-instagram" />
+                </div>
+            </div>
         </div>
     )
 }
