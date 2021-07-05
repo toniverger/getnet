a522b869ccb74ca856dadca4e97cfb3c1c7096c9
carolainseija <carolainsilva1@gmail.com>
2021-07-05 12:29:48 -0300

    page validtae, adress, docs and review - add images svg and btn select

---
+++ src/components/Buttons/select/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+export const ButtonsSelect = ({ text }) => {
+    return (
+        <button className="btn-selected">{text}</button>
+    )
+}
--- src/pages/Review.js
+++ src/pages/Review.js
@@ -5,26 +5,37 @@ import tel from '../images/icon-tel-white.svg';
 import instagram from '../images/icon-ig-white.svg';
 import facebook from '../images/icon-fb-white.svg';
 import linkedin from '../images/icon-in-white.svg';
+import group from '../images/Group.svg';
 
 const Review = () => {
     return (
         <div className="page-review">
+            <div className="background-img_left">
+                <img className="img-group" src={group} alt="img-group-208" />
+            </div>
             <div className="review">
                 <h1 className="white congrats ">¡Felicidades! <br></br> Estamos revisando tus datos</h1>
                 <p className="delay white font18">Esto puede demorar entre 24 y 48 horas.</p>
                 <p className="delay white font18">Cuando esté todo listo te confirmaremos por las vías de contacto solicitadas.</p>
-                <button className="btn-back">
-                    <Link to="/" className="white"> 
-                    Volver al inicio
-                    </Link></button>
+                <div className="review_content-btn">
+                    <button className="btn-back">
+                        <Link to="/" className="link-back white">
+                            Volver al inicio
+                        </Link></button>
+                </div>
             </div>
             <div className="review-contact">
                 <div className="questions">
                     <p className="white font18">¿Tenés dudas? Contactanos al</p>
-                    <img src={wpp} alt="icon-whatsApp" />
-                    <p className="tel white font18">0000 00 00</p>
-                    <img src={tel} alt="icon-telphone" />
-                    <p className="tel white font18">0000 00 00</p>
+                    <div className="review_contact">
+                        <img src={tel} alt="icon-telphone" />
+                        <p className="tel white font18">0000 00 00</p>
+                    </div>
+                    <div className="review_contact">
+                        <img src={wpp} alt="icon-whatsApp" />
+                        <p className="tel white font18">0000 00 00</p>
+                    </div>
+
                 </div>
                 <div className="socials">
                     <p className="white font18">Seguinos</p>
@@ -32,6 +43,9 @@ const Review = () => {
                     <img className="img-socials" src={linkedin} alt="icon-linkedin" />
                     <img className="img-socials" src={instagram} alt="icon-instagram" />
                 </div>
+            </div>
+            <div className="background-img_rigth">
+                <img className="img-group" src={group} alt="img-group-208" />
             </div>
         </div>
     )
