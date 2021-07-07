525b48b3c049c348b604fe0c262b146b8f62cc92
carolainseija <carolainsilva1@gmail.com>
2021-07-07 10:00:21 -0300

    steps hr responsive

--- src/components/Steps/Steps.js
+++ src/components/Steps/Steps.js
@@ -2,20 +2,21 @@ import React from "react";
 import "./style.css";
 
 
-const Steps = ({text1, text2, text3, src1, src2, src3, class_1, class_2, class_3}) => {
+const Steps = ({ text1, text2, text3, src1, src2, src3, class_1, class_2, class_3 }) => {
   return (
     <div className="Steps">
       <div className="encloseSteps">
         <img src={src1} />
-        <p className={class_1}> {text1} </p>
+        <p id="hidden-resp" className={class_1}> {text1} </p>
       </div>
+      <hr className="hr-step"></hr>
       <div className="encloseSteps">
         <img src={src2} />
-        <p className={class_2}> {text2} </p>
-      </div>
+        <p id="hidden-resp" className={class_2}> {text2} </p>
+      </div> <hr className="hr-step"></hr>
       <div className="encloseSteps">
         <img src={src3} />
-        <p className={class_3}> {text3} </p>
+        <p id="hidden-resp" className={class_3}> {text3} </p>
       </div>
     </div>
   );
