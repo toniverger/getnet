aa2649d9d4751a0db553df1f7b9eec4afc49743f
carolainseija <carolainsilva1@gmail.com>
2021-07-08 12:58:54 -0300

    pages docs, confirm, and component butons, edit styles globals

--- src/components/Buttons/select/Button.js
+++ src/components/Buttons/select/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './style.css';
 
-export const ButtonsSelect = ({ text }) => {
+export const ButtonsSelect = ({ text, className }) => {
     return (
-        <button className="btn-selected">{text}</button>
+        <button className={className}>{text}</button>
     )
 }
