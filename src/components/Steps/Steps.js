8df85714a49240c4c8a5592bd18c0f2fce9214a6
toniverger <toniverger@msn.com>
2021-07-19 18:49:12 -0300

    Change icons

--- src/components/Steps/Steps.js
+++ src/components/Steps/Steps.js
@@ -7,21 +7,21 @@ const Steps = ({ text1, text2, text3, src1, src2, src3, class_1, class_2, class_
   return (
     <div className="Steps">
       <div className="encloseSteps">
-        <img src={src1} />
+        <img src={src1} className="stepsIcons"/>
         <p id="hidden-resp" className={class_1}> {text1} </p>
       </div>
       <div className="content-hr_step">
         <hr className="hr-step"></hr>
       </div>
       <div className="encloseSteps">
-        <img src={src2} />
+        <img src={src2} className="stepsIcons"/>
         <p id="hidden-resp" className={class_2}> {text2} </p>
       </div>
       <div className="content-hr_step">
         <hr className="hr-step"></hr>
       </div>
       <div className="encloseSteps">
-        <img src={src3} />
+        <img src={src3} className="stepsIcons"/>
         <p id="hidden-resp" className={class_3}> {text3} </p>
       </div>
     </div>
